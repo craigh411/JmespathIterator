091a276aff0f286368eabfe8937be3ee0ed53db4
craig.humphreys.ai@gmail.com <craig.humphreys.ai@gmail.com>
2021-02-11 12:58:14 +0000

    iterator implemented

--- src/JmespathIterator.php
+++ src/JmespathIterator.php
@@ -5,57 +5,104 @@ namespace Humps\Jmespath;
 use ArrayAccess;
 use Countable;
 use Iterator;
+use Serializable;
 use function JmesPath\search;
 
-class JmespathIterator implements ArrayAccess, Countable, Iterator
+class JmespathIterator implements ArrayAccess, Countable, Iterator, Serializable
 {
 
+    /**
+     * @var array $arr
+     */
     private $arr;
+    /**
+     * @var int $index
+     */
+    private $index;
 
+    /**
+     * JmespathIterator constructor.
+     * @param array $arr
+     */
     function __construct(array $arr = [])
     {
         $this->arr = $arr;
+        $this->index = 0;
     }
 
+    /**
+     * @return mixed
+     */
     public function current()
     {
-        // TODO: Implement current() method.
+        $value = $this->arr[$this->index];
+        return (is_array($value)) ? new static($value) : $value;
     }
 
+    /**
+     *
+     */
     public function next()
     {
-        // TODO: Implement next() method.
+        $this->index++;
     }
 
+    /**
+     * @return int
+     */
     public function key()
     {
-        // TODO: Implement key() method.
+        return $this->index;
     }
 
+    /**
+     * @return bool
+     */
     public function valid()
     {
-        // TODO: Implement valid() method.
+        return isset($this->arr[$this->index]);
     }
 
+    /**
+     *
+     */
     public function rewind()
     {
-        // TODO: Implement rewind() method.
+        $this->index = 0;
     }
 
+    /**
+     * @param mixed $offset
+     * @return bool
+     */
     public function offsetExists($offset)
     {
-        // TODO: Implement offsetExists() method.
+        if (is_numeric($offset)) {
+            return isset($this->arr[$offset]);
+        }
+
+        return !!search($this->parseExpression($offset), $this->arr);
     }
 
+    /**
+     * @param mixed $expression
+     * @return mixed
+     */
     public function offsetGet($expression)
     {
         if (is_numeric($expression)) {
-            return $this->arr[$expression];
+            $value = $this->arr[$expression];
+            return (is_array($value)) ? new static($value) : $value;
         }
 
-        return search($this->parseExpression($expression), $this->arr);
+        $search = search($this->parseExpression($expression), $this->arr);
+        return is_array($search) ? new static($search) : $search;
     }
 
+    /**
+     * @param mixed $offset
+     * @param mixed $value
+     */
     public function offsetSet($offset, $value)
     {
         if (is_null($offset)) {
@@ -65,22 +112,64 @@ class JmespathIterator implements ArrayAccess, Countable, Iterator
         }
     }
 
+    /**
+     * @param mixed $offset
+     */
     public function offsetUnset($offset)
     {
-        // TODO: Implement offsetUnset() method.
+        unset($this->arr[$offset]);
     }
 
+    /**
+     * @return int
+     */
     public function count()
     {
         return count($this->arr);
     }
 
+
+    /**
+     * @return $this
+     */
+    public function values()
+    {
+        return new static(array_values($this->arr));
+    }
+
     /**
      * @return string
      */
-    public function __toString(): string
+    public function __toString()
     {
         return json_encode($this->arr, JSON_PRETTY_PRINT);
+    }
+
+    /**
+     * @return array
+     */
+    public function toArray()
+    {
+        return $this->arr;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function serialize()
+    {
+        return serialize([$this->arr, $this->index]);
+    }
+
+    /**
+     * @param string $serialized
+     */
+    public function unserialize($serialized)
+    {
+        [
+            $this->arr,
+            $this->index,
+        ] = unserialize($serialized);
     }
 
     /**
--- tests/JmespathIteratorTest.php
+++ tests/JmespathIteratorTest.php
@@ -32,7 +32,7 @@ class JmespathIteratorTest extends TestCase
     {
         $iterator = new JmespathIterator(['foo', 'bar', 'baz']);
         $iterator[] = 'qux';
-        $this->assertEquals($iterator['3'], 'qux');
+        $this->assertEquals($iterator[3], 'qux');
     }
 
     /**
@@ -60,7 +60,7 @@ class JmespathIteratorTest extends TestCase
     /**
      * @test
      */
-    public function it_returns_all_values_when_star_is_passed()
+    public function it_returns_all_values_when_asterisk_is_passed()
     {
         $iterator = new JmespathIterator(['foo', 'bar', 'baz', 'qux', 'quxx']);
 
@@ -144,5 +144,107 @@ class JmespathIteratorTest extends TestCase
         $this->assertEquals(3, count($iterator['[0::2]']));
 
         $this->assertEquals($iterator['0::2'], $iterator['[0::2]']);
+    }
+
+    /**
+     * @test
+     */
+    public function it_can_unset_a_value()
+    {
+        $iterator = new JmespathIterator(['foo', 'bar', 'baz', 'qux', 'quxx']);
+        unset($iterator[0]);
+
+        $this->assertEquals(4, count($iterator));
+
+        $this->assertEquals('bar', array_values($iterator->toArray())[0]);
+    }
+
+    /**
+     * @test
+     */
+    public function it_can_serialize_the_object()
+    {
+        $iterator = new JmespathIterator(['foo', 'bar', 'baz', 'qux', 'quxx']);
+        $serialized = serialize($iterator);
+        $this->assertEquals($iterator, unserialize($serialized));
+    }
+
+    /**
+     * @test
+     */
+    public function it_can_be_iterated_over()
+    {
+        $iterator = new JmespathIterator(['foo', 'bar']);
+        foreach ($iterator as $key => $value) {
+            $this->assertEquals($iterator[$key], $value);
+        }
+    }
+
+    /**
+     * @test
+     */
+    public function it_returns_a_jmespath_iterator_when_value_is_an_array_when_accessed_by_key()
+    {
+        $iterator = new JmespathIterator(['foo' => [
+            'bar' => [
+                'baz' => 'qux',
+            ],
+        ]]);
+
+        $this->assertInstanceOf(JmespathIterator::class, $iterator['foo']);
+        $this->assertEquals('qux', $iterator['foo']['bar.baz']);
+    }
+
+    /**
+     * @test
+     */
+    public function it_returns_a_jmespath_iterator_when_value_is_an_array_when_accessed_by_index()
+    {
+        $iterator = new JmespathIterator([
+            [
+                'bar' => [
+                    'baz' => 'qux',
+                ],
+            ],
+        ]);
+
+        $this->assertInstanceOf(JmespathIterator::class, $iterator[0]);
+        $this->assertEquals('qux', $iterator[0]['bar.baz']);
+    }
+
+    /**
+     * @test
+     */
+    public function it_can_check_that_a_numeric_offset_exists()
+    {
+        $iterator = new JmespathIterator(['foo', 'bar', 'baz']);
+
+        $this->assertTrue(isset($iterator[0]));
+        $this->assertFalse(isset($iterator[3]));
+    }
+
+    /**
+     * @test
+     */
+    public function it_can_check_that_a_non_numeric_offset_exists()
+    {
+        $iterator = new JmespathIterator(['foo' => 'bar']);
+
+        $this->assertTrue(isset($iterator['foo']));
+        $this->assertFalse(isset($iterator['bar']));
+    }
+
+    /**
+     * @test
+     */
+    public function it_can_check_that_a_jmespath_expression_returns_a_result()
+    {
+        $iterator = new JmespathIterator(['foo' => [
+            'bar' => 'baz',
+            ],
+        ]);
+
+        $this->assertTrue(isset($iterator['foo.bar']));
+        $this->assertFalse(isset($iterator['foo.bar.baz']));
     }
 }
